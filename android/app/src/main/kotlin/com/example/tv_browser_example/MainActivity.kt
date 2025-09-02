18956245e078f1377694039e63b505001daae0fc
kriztpow <45737457+kriztpow@users.noreply.github.com>
2025-09-02 02:45:31 -0400

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/tv_browser_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tv_browser_example/MainActivity.kt
@@ -2,5 +2,5 @@ package com.example.tv_browser_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends FlutterActivity() {
 }
